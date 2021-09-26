769cd4323f8a4345b9236352737b809e3b30bf22
monday solomon <monday_solomon01@gmail.com>
2021-09-26 15:49:23 +0000

    first commit

---
+++ user/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.urls import path
+from django.urls.conf import include
+from . import views
+
+urlpatterns = [
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path("accounts/", include('django.contrib.auth.urls')),
+]
